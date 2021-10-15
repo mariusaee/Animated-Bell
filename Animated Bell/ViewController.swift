4470014412bb98bd44675b7808f9e8b571c8f76e
mariusaee <mariusae@me.com>
2021-10-15 16:52:38 +0300

    Initial Commit

---
+++ Animated Bell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animated Bell
+//
+//  Created by Marius Malyshev on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
