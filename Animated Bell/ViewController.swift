45de5ce2ff53aa5a5d1bf6a601d3adf59c2cf324
mariusaee <mariusae@me.com>
2021-10-15 18:51:20 +0300

    Setup VC and Animation config

--- Animated Bell/ViewController.swift
+++ Animated Bell/ViewController.swift
@@ -8,12 +8,118 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bellView: UIImageView!
+    
+    var duration: Double = 1 {
+        didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
+    }
+    
+    var angle: CGFloat = .pi/8 {
+        didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
+    }
+    
+    var yOffset: CGFloat = 1 {
+        didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
-
-
+    
 }
 
+// MARK: - Setup
+extension ViewController {
+    func setup() {
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_: )))
+        bellView.addGestureRecognizer(singleTap)
+        bellView.isUserInteractionEnabled = true
+    }
+}
+
+
+// MARK: - Animation
+extension ViewController {
+    @objc func imageViewTapped(_ recognizer: UITapGestureRecognizer) {
+        shakeWith(duration: duration, angle: angle, yOffset: yOffset)
+    }
+    
+    private func shakeWith(duration: Double, angle: CGFloat, yOffset: CGFloat) {
+        print("duration: \(duration) angle: \(angle) offset: \(yOffset)")
+        
+        let numberOfFrames: Double = 6
+        let frameDuration = Double(1/numberOfFrames)
+        
+        bellView.setAnchorPoint(CGPoint(x: 0.5, y: yOffset))
+        
+        print("anchorPoint: \(bellView.layer.anchorPoint)")
+        
+        UIView.animateKeyframes(
+            withDuration: duration,
+            delay: 0,
+            options: [],
+            animations:{
+                UIView.addKeyframe(withRelativeStartTime: 0.0,
+                                   relativeDuration: frameDuration)
+                {
+                    self.bellView.transform = CGAffineTransform(rotationAngle: -angle)
+                    
+                }
+                
+                UIView.addKeyframe(withRelativeStartTime: frameDuration,
+                                   relativeDuration: frameDuration) {
+                    self.bellView.transform = CGAffineTransform(rotationAngle: +angle)
+                }
+                
+                UIView.addKeyframe(withRelativeStartTime: frameDuration*2,
+                                   relativeDuration: frameDuration) {
+                    self.bellView.transform = CGAffineTransform(rotationAngle: -angle)
+                }
+                
+                UIView.addKeyframe(withRelativeStartTime: frameDuration*3,
+                                   relativeDuration: frameDuration) {
+                    self.bellView.transform = CGAffineTransform(rotationAngle: +angle)
+                }
+                
+                UIView.addKeyframe(withRelativeStartTime: frameDuration*4,
+                                   relativeDuration: frameDuration) {
+                    self.bellView.transform = CGAffineTransform(rotationAngle: -angle)
+                }
+                
+                UIView.addKeyframe(withRelativeStartTime: frameDuration*5,
+                                   relativeDuration: frameDuration) {
+                    self.bellView.transform = CGAffineTransform.identity
+                }
+            }
+        )
+    }
+}
+
+extension UIView {
+    func setAnchorPoint(_ point: CGPoint) {
+        var newPoint = CGPoint(
+            x: bounds.size.width * point.x,
+            y: bounds.size.height * point.y
+        )
+        var oldPoint = CGPoint(
+            x: bounds.size.width * layer.anchorPoint.x,
+            y: bounds.size.height * layer.anchorPoint.y
+        )
+        
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+        
+        var position = layer.position
+        
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        layer.position = position
+        layer.anchorPoint = point
+    }
+}
