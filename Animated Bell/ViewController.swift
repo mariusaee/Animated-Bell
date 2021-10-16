e3b94e9a360eab8404b86ce6963c540ec8385931
mariusaee <mariusae@me.com>
2021-10-16 13:01:41 +0300

    Start adding rotation

--- Animated Bell/ViewController.swift
+++ Animated Bell/ViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var bellView: UIImageView!
+    @IBOutlet weak var durationSlider: UISlider!
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var offsetSlider: UISlider!
     
     var duration: Double = 1 {
         didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
     }
-    
     var angle: CGFloat = .pi/8 {
         didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
     }
-    
     var yOffset: CGFloat = 1 {
         didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
     }
+    var rotation: CGFloat = 1 {
+        didSet { shakeWith(duration: duration, angle: angle, yOffset: yOffset) }
+    }
+    
+//    let degrees: CGFloat = 20.0 //the value in degrees
+//    let radians: CGFloat = degrees * (.pi / 180)
+//    imageView.transform = CGAffineTransform(rotationAngle: radians)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        bellView.transform = CGAffineTransform(rotationAngle: 10)
     }
     
+    @IBAction func durationSliderChanged(_ sender: UISlider) {
+        duration = Double(sender.value)*2
+    }
+    @IBAction func angleSliderChanged(_ sender: UISlider) {
+        angle = CGFloat(sender.value) * .pi/2
+    }
+    @IBAction func offsetSliderChanged(_ sender: UISlider) {
+        yOffset = CGFloat(sender.value)
+    }
 }
 
 // MARK: - Setup
